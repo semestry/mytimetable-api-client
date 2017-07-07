9eea7778f07ed35a1a108baec3e5b0bdaf5f205e
Marco Krikke <marco@eveoh.nl>
2017-07-07 09:58:19 +0200

    Remove property file support in configuration

--- src/main/java/nl/eveoh/mytimetable/apiclient/configuration/Configuration.java
+++ src/main/java/nl/eveoh/mytimetable/apiclient/configuration/Configuration.java
@@ -18,7 +18,6 @@ package nl.eveoh.mytimetable.apiclient.configuration;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * Generic Configuration object.
@@ -27,14 +26,6 @@ import java.util.Properties;
  * @author Erik van Paassen
  */
 public class Configuration {
-
-    private static final String API_ENDPOINT_URIS = "apiEndpointUris";
-    private static final String API_KEY = "apiKey";
-    private static final String API_SSL_CN_CHECK = "apiSslCnCheck";
-    private static final String API_CONNECT_TIMEOUT = "apiConnectTimeout";
-    private static final String API_SOCKET_TIMEOUT = "apiSocketTimeout";
-    private static final String API_MAX_CONNECTIONS = "apiMaxConnections";
-    private static final String API_ENABLE_GZIP = "apiEnableGzip";
 
     /**
      * Key used for communicating with the MyTimetable API.
@@ -78,43 +69,13 @@ public class Configuration {
 
     /**
      * Enable Gzip compression
-     *
+     * <p>
      * Defaults to true
      */
     private boolean apiEnableGzip = true;
 
 
-    public Configuration() {}
-
-    public Configuration(Properties properties) {
-        apiKey = properties.getProperty(API_KEY);
-
-        String uris = properties.getProperty(API_ENDPOINT_URIS);
-        if (uris != null) {
-            for (String uri : uris.split("\n")) {
-                uri = uri.trim();
-
-                if (!uri.isEmpty()) {
-                    apiEndpointUris.add(uri);
-                }
-            }
-        }
-
-        apiSslCnCheck = Boolean.parseBoolean(properties.getProperty(API_SSL_CN_CHECK));
-
-        try {
-            apiConnectTimeout = Integer.parseInt(properties.getProperty(API_CONNECT_TIMEOUT));
-        } catch (NumberFormatException e) { /* Do nothing, keep default value. */ }
-
-        try {
-            apiSocketTimeout = Integer.parseInt(properties.getProperty(API_SOCKET_TIMEOUT));
-        } catch (NumberFormatException e) { /* Do nothing, keep default value. */ }
-
-        try {
-            apiMaxConnections = Integer.parseInt(properties.getProperty(API_MAX_CONNECTIONS));
-        } catch (NumberFormatException e) { /* Do nothing, keep default value. */ }
-
-        apiEnableGzip = Boolean.parseBoolean(properties.getProperty(API_ENABLE_GZIP));
+    public Configuration() {
     }
 
     public String getApiKey() {
@@ -171,36 +132,5 @@ public class Configuration {
 
     public void setApiEnableGzip(boolean apiEnableGzip) {
         this.apiEnableGzip = apiEnableGzip;
-    }
-
-    /**
-     * Creates a {@link Properties} object containing the configuration values.
-     *
-     * @return  Properties object
-     */
-    public Properties toProperties() {
-        Properties ret = new Properties();
-
-        StringBuilder uris = new StringBuilder();
-        for (String apiEndpointUri : apiEndpointUris) {
-            if (uris.length() > 0) {
-                uris.append('\n');
-            }
-
-            uris.append(apiEndpointUri);
-        }
-        ret.setProperty(API_ENDPOINT_URIS, uris.toString());
-
-        ret.setProperty(API_SSL_CN_CHECK, String.valueOf(apiSslCnCheck));
-        ret.setProperty(API_CONNECT_TIMEOUT, String.valueOf(apiConnectTimeout));
-        ret.setProperty(API_SOCKET_TIMEOUT, String.valueOf(apiSocketTimeout));
-        ret.setProperty(API_MAX_CONNECTIONS, String.valueOf(apiMaxConnections));
-        ret.setProperty(API_ENABLE_GZIP, String.valueOf(apiEnableGzip));
-
-        if (apiKey != null) {
-            ret.setProperty(API_KEY, apiKey);
-        }
-
-        return ret;
     }
 }
